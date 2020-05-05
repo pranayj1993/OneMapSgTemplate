1096bb96b94f5aa2c6f414a7e9184013694377c8
pranayj1993 <62631134+pranayj1993@users.noreply.github.com>
2020-05-05 17:12:02 +0800

    SW and workbox changes
    changes for workbox storage

--- WebContent/sw.js
+++ WebContent/sw.js
@@ -12,7 +12,7 @@
  */
 
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("workbox-sw.js");
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
